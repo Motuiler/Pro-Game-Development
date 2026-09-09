a994e5f2cf90b5653c794c89a0ca30db1d1748c6
Motuiler <ekaansh.keshri@gmail.com>
2026-09-09 16:53:35 +0200

    Add files via upload

--- Space Invaders/spaceinvaders.py
+++ Space Invaders/spaceinvaders.py
@@ -11,6 +11,7 @@ background=pygame.image.load(r'C:\Users\Ekaansh\Desktop\Coding Jetlearn\Pro Game
 background=pygame.transform.scale(background,(WIDTH,HEIGHT))
 SIZE=50
 SPEED=3
+BULLETSPEED=5
 red=pygame.image.load(r'C:\Users\Ekaansh\Desktop\Coding Jetlearn\Pro Game Development\Space Invaders\assets\spaceship red.png')
 red=pygame.transform.scale(red,(SIZE,SIZE))
 red=pygame.transform.rotate(red,90)
@@ -19,6 +20,7 @@ yellow=pygame.image.load(r'C:\Users\Ekaansh\Desktop\Coding Jetlearn\Pro Game Dev
 yellow=pygame.transform.scale(yellow,(SIZE,SIZE))
 yellow=pygame.transform.rotate(yellow,270)
 yellowrect=pygame.Rect(750,250,SIZE,SIZE)
+silencer=pygame.mixer.Sound(r'C:\Users\Ekaansh\Desktop\Coding Jetlearn\Pro Game Development\Space Invaders\assets\Silencer+Gun Sound.mp3')
 
 border=pygame.Rect(WIDTH/2-10,0,20,HEIGHT)
 clock=pygame.time.Clock()
@@ -43,7 +45,19 @@ def yellowspaceshipmovement(keys):
     if keys[pygame.K_RIGHT] and yellowrect.right<WIDTH:
         yellowrect.x=yellowrect.x+SPEED
 
+def handlebullet(redbullet,yellowbullet):
+    for bullet in redbullet:
+        bullet.x=bullet.x+BULLETSPEED
+        if bullet.x>WIDTH:
+            redbullet.remove(bullet)
+    for bullet in yellowbullet:
+        bullet.x=bullet.x-BULLETSPEED
+        if bullet.x<0:
+            yellowbullet.remove(bullet)
+
 def restart():
+    redbullet=[]
+    yellowbullet=[]  
     run=True
     while run:
         clock.tick(FPS)
@@ -51,15 +65,29 @@ def restart():
         window.blit(red,(redrect.x,redrect.y))
         window.blit(yellow,(yellowrect.x,yellowrect.y))
         pygame.draw.rect(window,(0,0,0),border)
+        for bullet1 in redbullet:
+            pygame.draw.rect(window,(255,255,255),bullet1)
+        for bullet2 in yellowbullet:
+            pygame.draw.rect(window,(255,255,255),bullet2)
 
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
                 pygame.quit()
 
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_LSHIFT:
+                    bullet1=pygame.Rect(redrect.right,redrect.y+redrect.height/2,10,5)
+                    redbullet.append(bullet1)
+                    silencer.play()
+                if event.key==pygame.K_KP0:
+                    bullet2=pygame.Rect(yellowrect.left,yellowrect.y+yellowrect.height/2,10,5)
+                    yellowbullet.append(bullet2)
+                    silencer.play()
         keys=pygame.key.get_pressed()
         redspaceshipmovement(keys)
         yellowspaceshipmovement(keys)
+        handlebullet(redbullet,yellowbullet)
         pygame.display.update()
 
 
